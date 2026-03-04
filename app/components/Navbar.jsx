4e054d740ae4d05fcddbf77fce8ce9d8978e5719
Airam Pérez <131189091+airamprz@users.noreply.github.com>
2026-03-04 21:05:12 +0100

    Fourth Commit

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -2,7 +2,7 @@ export default function Navbar({ fontClass }) {
   return (
     <nav className="w-full bg-black h-32 flex items-center justify-center">
       <span className={`${fontClass} text-white text-[36px] leading-none select-none`}>
-        PAIN B4 PLEASURE 
+        PAIN BEFORE PLEASURE 
       </span>
     </nav>
   );
