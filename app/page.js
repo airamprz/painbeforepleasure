e8e20ee2dfe27a2b7637d33a1c97bda7902c8e30
Airam Pérez <131189091+airamprz@users.noreply.github.com>
2026-03-05 15:10:18 +0100

    New commit 2

--- app/page.js
+++ app/page.js
@@ -3,15 +3,18 @@ import Image from "next/image";
 const brands = [
   { name: "Ooski Music Records", url: "https://ooski.bigcartel.com", logo: "/brands/ooski.png" },
   { name: "Honey Money Entertainment", url: "https://www.honeymoneyent.com", logo: "/brands/honeymoney.png" },
+  { name: "XVSTRANGE", url: "https://xvstrange.com", logo: "/brands/xvstrange.png" },
+
+  { name: "Moneychaserszzz", url: "https://www.instagram.com/moneychaserszzz/", logo: "/brands/moneychaserszzz.png" },
+
+  { name: "losdiablosofficial", url: "https://www.instagram.com/losdiablosofficial/", logo: "/brands/losdiablosnew.png" },
   { name: "Real Motion Cartel", url: "https://www.realmotioncartel.com", logo: "/brands/realmotioncartel.png" },
 
   { name: "4daho3s.mob", url: "https://www.instagram.com/4daho3s.mob/", logo: "/brands/4daho3s.mob.png" },
-  { name: "losdiablosofficial", url: "https://www.instagram.com/losdiablosofficial/", logo: "/brands/losdiablosnew.png" },
-  { name: "XVSTRANGE", url: "https://xvstrange.com", logo: "/brands/xvstrange.png" },
-
+  { name: "Trapaholikzzz", url: "https://www.instagram.com/Trapaholikzzz/", logo: "/brands/trapaholikzzz.png" },
   { name: "TrueCbby", url: "https://linktr.ee/TrueCbby", logo: "/brands/truecbbynew.png" },
-  { name: "Trapaholikzzz", url: "https://www.instagram.com/Trapaholikzzz/", logo: "/brands/Trapaholikzzz.png" },
-  { name: "Moneychaserszzz", url: "https://www.instagram.com/moneychaserszzz/", logo: "/brands/moneychaserszzz.png" },
+
+
 ];
 
 export default function Home() {
